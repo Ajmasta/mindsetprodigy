f2ceb793f017492936ade8658880fbb98c00960a
ajmastaflex <adrien.moevus@gmail.com>
2020-07-26 17:37:37 -0400

    Updates

---
+++ precache-manifest.4383fc14550b07683fc6b61841e084cd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2808ec4d5f45b4f843f303369fd9c366",
+    "url": "/mindsetprodigy/index.html"
+  },
+  {
+    "revision": "398afc5eea2dba817008",
+    "url": "/mindsetprodigy/static/css/2.a24412fc.chunk.css"
+  },
+  {
+    "revision": "0f81be83ebf6c58d00bd",
+    "url": "/mindsetprodigy/static/css/main.12392aec.chunk.css"
+  },
+  {
+    "revision": "398afc5eea2dba817008",
+    "url": "/mindsetprodigy/static/js/2.d8954999.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/mindsetprodigy/static/js/2.d8954999.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f81be83ebf6c58d00bd",
+    "url": "/mindsetprodigy/static/js/main.07dff872.chunk.js"
+  },
+  {
+    "revision": "ce46314d45ea0962ccae",
+    "url": "/mindsetprodigy/static/js/runtime-main.b9dc75b0.js"
+  },
+  {
+    "revision": "ca65b211bca1dd2c6f225db8af9586c7",
+    "url": "/mindsetprodigy/static/media/SpriteGraffiti-Regular.ca65b211.ttf"
+  },
+  {
+    "revision": "741e3384e176e69fdf64903fc3e11b2f",
+    "url": "/mindsetprodigy/static/media/background.741e3384.mp4"
+  },
+  {
+    "revision": "1e670d88b23c7ab956f1829e3828a210",
+    "url": "/mindsetprodigy/static/media/digital-7.1e670d88.ttf"
+  },
+  {
+    "revision": "065527458bf16f7de72c752d51bf94f7",
+    "url": "/mindsetprodigy/static/media/jupiter2.06552745.mp3"
+  }
+]);
